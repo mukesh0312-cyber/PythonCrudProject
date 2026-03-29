4a4c98f3e26177858871d34bf45f51b03d222229
Mukesh Kumar <mukeshk5823@gmail.com>
2026-03-29 12:44:49 +0530

    Final fix MySQL CI issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,13 +81,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #     }
 # }
 
-
+if os.getenv('GITHUB_ACTIONS'):
+    DB_PASSWORD = 'root'
+else:
+    DB_PASSWORD = ''
+    
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_project',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': DB_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
